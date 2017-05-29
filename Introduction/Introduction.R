f3d7f4d4f5d9ffb10f0c692c097dcba1122531cc
Mario Antonioletti <mario@epcc.ed.ac.uk>
2017-05-29 08:26:52 +0100

    Adding an introduction.

---
+++ Introduction/Introduction.R
@@ -0,0 +1,15 @@
+# Introduction to R
+#
+# This is a comment, anything that follows a hash to the end of the line will be ignored.
+# To execute commands type:
+#                           Cnt-Enter on Windows and Linux
+#                           Cmd-Enter on Macs
+
+# Using R as a calculator.
+1+100
+
+# What follows is an incomplete command - to complete the command either complete it
+# or type Esc to abort
+1+
+
+
