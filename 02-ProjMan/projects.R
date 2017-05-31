b3392f883b4a171818c328cc5daeb3d96aa0f3e2
Mario Antonioletti <mario@epcc.ed.ac.uk>
2017-05-31 08:03:37 +0100

    Instructions to create a new project.

---
+++ 02-ProjMan/projects.R
@@ -0,0 +1,9 @@
+# Creating a new project in R studio:
+#
+# 1. Click the “File” menu button, then “New Project”.
+# 2. Click “New Directory”.
+# 3. Click “Empty Project”.
+# 4. Type in the name of the directory to store your project, e.g. “my_project”.
+# 5. Make sure that the checkbox for “Create a git repository” is selected.
+# 6. Click the “Create Project” button.
+#
