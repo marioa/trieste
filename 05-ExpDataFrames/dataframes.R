169f0ce43606da3b2a64e3121d84f2a80ba56f3c
Mario Antonioletti <mario@epcc.ed.ac.uk>
2017-06-07 16:43:29 +0100

    Content to explore data frames.

---
+++ 05-ExpDataFrames/dataframes.R
@@ -0,0 +1,45 @@
+# Exploring data frames
+
+# Adding columns and rows -------------------------------------------------
+
+# Want to add a new column of ages
+age <- c(2,3,5,12)
+cats
+
+# Add a column using cbind
+cats <- cbind(cats, age)   # Oops!
+ 
+# All columns (vectors) must be of the same length!
+age  <- c(4,5,8)
+cats <- cbind(cats, age)
+cats
+
+# Now try to add a new row.
+# Need to use a list as they are mised data types.
+newRow <- list("tortoiseshell", 3.3, TRUE, 9)
+cats <- rbind(cats, newRow)         # Oops!
+
+# coat is a factor
+str(cats)
+
+# Has a set of allowed entries, otherwise it will insert an NA.
+levels(cats$coat)
+
+# We need to add "toritoseshell" to the allowed entries
+levels(cats$coat) <- c(levels(cats$coat), 'tortoiseshell')
+levels(cats$coat)
+
+# Now we should be able to add the entry
+cats <- rbind(cats, list("tortoiseshell", 3.3, TRUE, 9))
+
+# Check
+str(cats)    # Oops!
+
+# Change the factor entry
+cats$coat <- as.character(cats$coat)
+str(cats)
+
+
+# Removing rows -----------------------------------------------------------
+
+
