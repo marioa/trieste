06e6e64c3cecd5a44bb8e873adf5217399d1583f
Mario Antonioletti <mario@epcc.ed.ac.uk>
2017-06-18 20:53:11 +0100

    Finished this section.

--- 10-Functions/functions.R
+++ 10-Functions/functions.R
@@ -67,6 +67,113 @@ fahr_to_kelvin(212)
 # Hint: To convert from Kelvin to Celsius you subtract 273.15
 
 
-# Combining functions -----------------------------------------------------
+# 1.2 Combining functions
+#     Let's define 2 new functions:
+
+fahr_to_kelvin <- function(temp) {
+  kelvin <- ((temp - 32) * (5 / 9)) + 273.15
+  return(kelvin)
+}
+
+kelvin_to_celsius <- function(temp) {
+  celsius <- temp - 273.15
+  return(celsius)
+}
+
+# Use these two functions to create a new fahr_to_celsius function.
+
+
+# Something ---------------------------------------------------------------
+
+# Define a function that calculates the Gross Domestic Product of a nation 
+# from the data available in our dataset:
+
+# Takes a dataset and multiplies the population column
+# with the GDP per capita column.
+calcGDP <- function(dat) {
+  gdp <- dat$pop * dat$gdpPercap
+  return(gdp)
+}
+
+# Lets run the function
+calcGDP(head(gapminder))
+
+# Not very informative, let's modify the function.
+
+# Takes a dataset and multiplies the population column
+# with the GDP per capita column.
+calcGDP <- function(dat, year=NULL, country=NULL) {
+  
+  if(!is.null(year)) {                # Extract the data for specific year
+    dat <- dat[dat$year %in% year, ]
+  }
+  if (!is.null(country)) {            # Extract data for a specific country
+    dat <- dat[dat$country %in% country,]
+  }
+  
+  gdp <- dat$pop * dat$gdpPercap # Calculate the gdp
+  
+  new <- cbind(dat, gdp=gdp) # Bind data to new column in data frame
+  
+  return(new)  # Return the new data frame
+}
+
+# If you put functions in a different script then you can enable them
+# by running:
+#
+# source(dirToFile/filename.R)
+#
+# You will then be able to run the files.
+
+
+# Let's look at the output now.
+head(calcGDP(gapminder, year=2007))
+
+# or for a specific country
+calcGDP(gapminder, country="Australia")
+
+# Or both:
+calcGDP(gapminder, year=2007, country="Australia")
+
+# Functions in R almost always make copies of the data to operate on 
+# inside of a function body. When we modify dat inside the function 
+# we are modifying the copy of the gapminder dataset stored in dat, 
+# not the original variable we gave as the first argument.
+
+# This is called “pass-by-value” and it makes writing code much safer: 
+# you can always be sure that whatever changes you make within the body 
+# of the function, stay inside the body of the function.
+
+
+
+# Challenge 2 -------------------------------------------------------------
+
+# 2.1 Test the GDP function by calculating the GDP for New Zealand 
+#     in 1987. How does this differ from New Zealand’s GDP in 1952?
+
+# 2.2 The paste function can be used to combine text together, e.g:
+
+best_practice <- c("Write", "programs", "for", "people", "not", "computers")
+paste(best_practice, collapse=" ")
+
+# Write a function called fence that takes two vectors as arguments, 
+# called text and wrapper, and prints out the text wrapped with the wrapper:
+#  
+#  fence(text=best_practice, wrapper="***")
+#
+# Note: the paste function has an argument called sep, which specifies the 
+# separator between text. The default is a space: “ “. The default for 
+# paste0 is no space “”.
+
+# Writing R functions:
+# When you first start out, your workflow will probably look a lot like this:
+#   
+# 1. Write a function
+# 2. Comment parts of the function to document its behaviour
+# 3. Load in the source file
+# 4. Experiment with it in the console to make sure it behaves as you expect
+# 5. Make any necessary bug fixes
+# 6. Rinse and repeat.
+#
 
 
