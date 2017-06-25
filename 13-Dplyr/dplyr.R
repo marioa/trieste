6383be6069373c0e218ae7ba4428cee14e03d57e
Mario Antonioletti <m.antonioletti@epcc.ed.ac.uk>
2017-06-25 19:19:02 +0100

    Finished adding dplyr content.

--- 13-Dplyr/dplyr.R
+++ 13-Dplyr/dplyr.R
@@ -55,7 +55,7 @@ year_country_gdp_euro <- gapminder %>%
 # Note that the %>%  must go at the end of the line if you are breaking up
 # into multiple lines. The following will give you an error:
 year_country_gdp_euro <- gapminder 
-%>% filter(continent=="Europe") %>% select(year,country,gdpPercap)
+%>% filter(continent=="Europe") 
 
 
 # Challenge 1 -------------------------------------------------------------
@@ -67,3 +67,123 @@ year_country_gdp_euro <- gapminder
 
 
 
+# Using group_by() and summarize() ----------------------------------------
+
+# Can only filter by one continent at a time. The group_by() allows us to
+# aggregate by continents.
+gapminder %>% group_by(continent)
+
+# We can then create new columns using summarize() - we can use functions
+# within summarize:
+(gdp_bycontinents <- gapminder %>%
+  group_by(continent) %>%
+  summarise(mean_gdpPercap=mean(gdpPercap)))
+
+# or we could do the mean life expectancy
+gapminder %>%
+  group_by(continent) %>%
+  summarise(meanLifeExp=mean(lifeExp))
+
+# Can also group by multiple variables - continent and year
+(gdp_bycontinents_byyear <- gapminder %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap=mean(gdpPercap)))
+
+# You can also summarise over multiple variables
+(gdp_pop_bycontinents_byyear <- gapminder %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap=mean(gdpPercap),
+            sd_gdpPercap=sd(gdpPercap),
+            mean_pop=mean(pop),
+            sd_pop=sd(pop)))
+
+# Challenge 2 -------------------------------------------------------------
+
+# 2.1 Calculate the average life expectancy per country. Which has the longest 
+#     average life expectancy and which has the shortest average life expectancy?
+
+# 2.2 Do the same thing but using the arrange() (arrange rows by variable)
+#     and desc() (descending order) functions.
+
+
+# count() and n() ---------------------------------------------------------
+
+# Common is to count observations for each group
+
+# Count the number of countries included in the dataset for the year 2002
+gapminder %>%
+  filter(year == 2002) %>%
+  count(continent, sort = TRUE)
+
+# If we need to use the number of observations in calculations, 
+# the n() function is useful. To get the standard error:
+gapminder %>%
+  group_by(continent) %>%
+  summarize(se_pop = sd(lifeExp)/sqrt(n()))
+
+
+# Using mutate() ----------------------------------------------------------
+
+# Use mutate to introduce new columns (variables)
+(gdp_pop_bycontinents_byyear <- gapminder %>%
+  mutate(gdp_billion=gdpPercap*pop/10^9) %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap=mean(gdpPercap),
+            sd_gdpPercap=sd(gdpPercap),
+            mean_pop=mean(pop),
+            sd_pop=sd(pop),
+            mean_gdp_billion=mean(gdp_billion),
+            sd_gdp_billion=sd(gdp_billion)))
+
+# Using ifelse
+ifelse(TRUE,"Do this","else do this")
+ifelse(FALSE,"Do this","else do this")
+
+## keeping all data but "filtering" after a certain condition
+# calculate GDP only for people with a life expectation above 25
+(gdp_pop_bycontinents_byyear_above25 <- gapminder %>%
+  mutate(gdp_billion= ifelse(lifeExp > 25, gdpPercap*pop/10^9, NA)) %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap=mean(gdpPercap),
+            sd_gdpPercap=sd(gdpPercap),
+            mean_pop=mean(pop),
+            sd_pop=sd(pop),
+            mean_gdp_billion=mean(gdp_billion),
+            sd_gdp_billion=sd(gdp_billion)))
+
+## updating only if certain condition is fullfilled
+# for life expectations above 40 years, the gpd to be expected in the future is scaled
+(gdp_future_bycontinents_byyear_high_lifeExp <- gapminder %>%
+  mutate(gdp_futureExpectation= ifelse(lifeExp > 40, gdpPercap * 1.5, gdpPercap)) %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap=mean(gdpPercap),
+            mean_gdpPercap_expected=mean(gdp_futureExpectation)))
+
+
+# Combining dplyr and ggplot2 ---------------------------------------------
+
+# We have seen this before:
+
+# Get the start letter of each country
+starts.with <- substr(gapminder$country, start = 1, stop = 1)
+
+# Filter countries that start with "A" or "Z"
+az.countries <- gapminder[starts.with %in% c("A", "Z"), ]
+
+# Make the plot
+ggplot(data = az.countries, aes(x = year, y = lifeExp, color = continent)) +
+  geom_line() + facet_wrap( ~ country)
+
+# We can now do this without introducing auxiliary arrays (starts.with and az.countries) :
+# (note that the data to be used by ggplot comes from the piping
+# as well so you do not have to introduce it)
+
+gapminder %>% 
+  # Get the start letter of each country 
+  mutate(startsWith = substr(country, start = 1, stop = 1)) %>% 
+  # Filter countries that start with "A" or "Z"
+  filter(startsWith %in% c("A", "Z")) %>%
+  # Make the plot
+  ggplot(aes(x = year, y = lifeExp, color = continent)) + 
+  geom_line() + 
+  facet_wrap( ~ country)
