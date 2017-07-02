ccb76d6e342536f4a6f222a6214499e373639a5a
Mario Antonioletti <m.antonioletti@epcc.ed.ac.uk>
2017-07-02 21:28:14 +0100

    Added some sql content.

---
+++ 16-RandSQL/sql.R
@@ -0,0 +1,21 @@
+
+# Introduction ------------------------------------------------------------
+
+
+# dplyr supports connecting to the widely-used open source databases:
+#
+# * sqlite, 
+# * mysql
+# * postgresql
+# * Google’s bigquery, 
+#
+# and it can also be extended to other database types 
+# (a vignette in the dplyr package explains how to do it).
+
+# dplyr allows you to access databases but not modify them
+# for that you need RSQLite, RMySQL, etc.
+
+
+# The portal_mammals database ---------------------------------------------
+
+
